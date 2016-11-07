90f2cfa77b39bfc8321eb800b7d9b95644abcf37
clarkerubber <jamesjosephclarke@gmail.com>
2016-11-08 04:10:43 +1100

    modify organise function

--- organise.py
+++ organise.py
@@ -9,6 +9,7 @@ import pickle
 from modules.bcolors.bcolors import bcolors
 from modules.api.api import get_player_data
 from modules.api.tools import get_files
+from operator import attrgetter
 
 """Start importing players"""
 def organise_training_data(token):
@@ -18,9 +19,10 @@ def organise_training_data(token):
         with open('test-data/saved/'+i, 'rb') as inputpkl:
             d = pickle.load(inputpkl)
             player_data = get_player_data(d.name, token)
+            processed = list([x for x in player_data['history'] if x['type'] == 'report'])[0].get('processedBy', None) is not None
             if player_data['assessment']['user']['engine']:
                 os.rename('test-data/saved/'+i, 'test-data/saved/cheaters/'+i)
-            elif player_data['history'][0]['data'].get('processedBy', None) is not None:
+            elif processed:
                 os.rename('test-data/saved/'+i, 'test-data/saved/legits/'+i)
 
 #organise_training_data(settings.token)
