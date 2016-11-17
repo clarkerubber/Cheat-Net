5d6d007edadcd315b4ef69d5ab5bbe5701f86aea
clarkerubber <jamesjosephclarke@gmail.com>
2016-11-18 00:36:28 +1100

    somehow solve cheating with tensorflow

--- tf_trainer.py
+++ tf_trainer.py
@@ -1,49 +1,65 @@
 import tensorflow as tf
-
+from modules.bcolors.bcolors import bcolors
 
 def combine_inputs(X):
-    player = tf.reshape(tf.slice(X, [0, 0], [-1, 5]), [-1, 5])
+    player = tf.reshape(tf.slice(X, [0, 0], [-1, 5]), [-1, 1, 1, 5])
     game = tf.reshape(tf.slice(X, [0, 5], [-1, 75]), [-1, 5, 15, 1])
-    conv = tf.contrib.layers.convolution2d(game, 4,
+
+    conv = tf.contrib.layers.convolution2d(game, 30,
         kernel_size=(1,15),
         stride=(1,15),
         activation_fn=tf.nn.relu,
+        weights_initializer=tf.random_normal,
+        biases_initializer=tf.random_normal,
         trainable=True)
-    #convout = tf.shape(tf.pack([tf.map_fn(lambda x: tf.reshape(x, [1, -1]), conv), player]))
 
-    rs = tf.reshape(conv, [-1, 20])
-    fn = tf.contrib.layers.fully_connected(rs, 5)
+    gamesfnn = tf.contrib.layers.stack(tf.reshape(conv, [-1, 1, 1, 150]), tf.contrib.layers.fully_connected, [100, 90, 80, 50])
 
-    return tf.contrib.layers.fully_connected(tf.concat(1, [player, fn]), 1)
+    playerfnn = tf.contrib.layers.fully_connected(
+        player,
+        50,
+        biases_initializer=tf.random_normal
+        )
+
+    playerandgamesfnn = tf.contrib.layers.stack(
+        tf.concat(3, [playerfnn, gamesfnn]),
+        tf.contrib.layers.fully_connected,
+        [100, 50, 10, 10, 2]
+        )
+
+    return tf.reshape(playerandgamesfnn, [-1, 2])
 
 def inference(X):
-    return tf.sigmoid(combine_inputs(X))
+    return tf.nn.softmax(combine_inputs(X))
 
 def loss(X, Y):
-    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(combine_inputs(X), Y))
+    comb = combine_inputs(X)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(comb, Y))
+    predicted = tf.round(tf.nn.softmax(comb))
+    evaluation = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), tf.float32))
+    return loss, evaluation, tf.concat(1, [predicted, Y, comb])
 
 def inputs():
-    #cheating,name,titled,closed_reports,blocker_to_followers,cheaters_to_legits,games_played,
-    #mb1,mb2,mb3,mb4,mb5,hb1,hb2,hb3,hb4,hb5,h1,h2,h3,h4,h5,m1,m2,m3,m4,m5,
-    #s1,s2,s3,s4,s5,r01,r02,r03,r04,r05,r11,r12,r13,r14,r15,r51,r52,r53,r54,r55,
-    #r020p1,r020p2,r020p3,r020p4,r020p5,r01220p1,r01220p2,r01220p3,r01220p4,r01220p5,
-    #cp201,cp202,cp203,cp204,cp205,cp101,cp102,cp103,cp104,cp105,cp1001,cp1002,cp1003,cp1004,cp1005,
-    #cpar11,cpar12,cpar13,cpar14,cpar15,cpar21,cpar22,cpar23,cpar24,cpar25
-    input_list = read_csv(200, [[0.0], [""], [0.0], [0.0], [0.0], [0.0], [0.0]] + [[0.0]] * 15*5)
+    input_list = read_csv(300, [[0.0], [""], [0.0], [0.0], [0.0], [0.0], [0.0]] + [[0.0]] * 15*5)
     features = tf.transpose(tf.pack(input_list[2:]))
-    cheating = tf.reshape(input_list[0], [200, 1])
+    cheat = tf.to_float(tf.equal(input_list[0], [1]))
+    legit = tf.to_float(tf.equal(input_list[0], [0]))
+    cheating = tf.transpose(tf.pack([legit, cheat]))
     return features, cheating
 
 def train(total_loss):
-    learning_rate = 0.01
-    return tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
+    learning_rate = 0.00001
+    #return tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
+    return tf.train.AdamOptimizer(learning_rate).minimize(total_loss)
 
-def evaluate(sess, X, Y):
-    predicted = tf.cast(inference(X) > 0.5, tf.float32)
-    print sess.run(tf.reduce_mean(tf.cast(tf.equal(predicted, Y), tf.float32)))
+def evaluate(X, Y):
+    with tf.name_scope("evaluate"):
+        predicted = tf.cast(inference(X) > 0.5, tf.float32)
+        loss = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), tf.float32))
+        return loss
     
 def read_csv(batch_size, record_defaults):
-    filename_queue = tf.train.string_input_producer(['test-data/player_data.csv'])
+    filename_queue = tf.train.string_input_producer(['test-data/player_data_balanced.csv'])
     reader = tf.TextLineReader(skip_header_lines=1)
     key, value = reader.read(filename_queue)
     decoded = tf.decode_csv(value, record_defaults=record_defaults)
@@ -53,30 +69,45 @@ def read_csv(batch_size, record_defaults):
         num_threads=4,
         min_after_dequeue=batch_size*10)
 
-config = tf.ConfigProto(inter_op_parallelism_threads=2)
-with tf.Session(config=config) as sess:
-    
-    X, Y = inputs()
-    ## initliase graph for running
-    tf.initialize_all_variables().run()
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    
-    total_loss = loss(X, Y)
-    train_op = train(total_loss)
-    training_steps = 10000
-    saver = tf.train.Saver()
-    for step in range(training_steps):
-        sess.run([train_op])
-        if step % 100 == 0:
-            print "loss: ", sess.run([total_loss])
+graph = tf.Graph()
+with graph.as_default():
+    with tf.Session(graph=graph) as sess:
+        X, Y = inputs()
+        ## initliase graph for running
+        with tf.name_scope("global_ops"):
+            writer = tf.train.SummaryWriter('./tf_graph', graph)
+            total_loss, evaluation, comp = loss(X, Y)
+            train_op = train(total_loss)
+            #test = combine_inputs(X)
+            
+            merged_summaries = tf.merge_all_summaries()
+            training_steps = 100000
+            saver = tf.train.Saver()
+            tf.initialize_all_variables().run()
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        #print "comp", sess.run([Y])
+        for step in range(training_steps):
+            sess.run([train_op])
+            if step % 100 == 0:
+                loss, eva, compar = sess.run([total_loss, evaluation, comp])
+                #writer.add_summary(summary, global_step=step)
+                for i in compar:
+                    if i[0] == i[2]:
+                        print bcolors.OKGREEN + str(i) + bcolors.ENDC
+                    else:
+                        print bcolors.FAIL + str(i) + bcolors.ENDC
+                print "loss: ", loss
+                print "eval: ", eva
+                print " "
+            
+            if step%1000 == 0:
+                saver.save(sess, 'my-model', global_step=step)
+        coord.request_stop()
+        coord.join(threads)
+        writer.flush()
+        writer.close()
+        saver.save(sess, 'my-model', global_step=training_steps)
         
-        if step%1000 == 0:
-            saver.save(sess, 'my-model', global_step=step)
-    
-    evaluate(sess, X, Y)
-    
-    saver.save(sess, 'my-model', global_step=training_steps)
-    coord.request_stop()
-    coord.join(threads)
-    sess.close()
+        sess.close()
