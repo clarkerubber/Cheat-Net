954bfc148872f47988431f0e5952964784df9ce8
James Clarke <jamesjosephclarke@gmail.com>
2016-12-25 14:04:41 +1100

    lower threshold a bit more

--- modules/analysis/AnalysedPlayer.py
+++ modules/analysis/AnalysedPlayer.py
@@ -64,7 +64,7 @@ class AnalysedPlayer:
         return (self.assess(), self.report())
 
     def assess(self):
-        if self.activation() > 2:
+        if self.activation() > 1:
             return True
         return False
 
